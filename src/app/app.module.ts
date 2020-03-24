fb09925ef695e40503a8d38b82e688c25a652a2a
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-24 18:16:17 +0100

    Http interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AuthInterceptService } from './auth-intercept.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptService, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/auth-intercept.service.ts
@@ -0,0 +1,19 @@
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler
+} from '@angular/common/http';
+
+export class AuthInterceptService implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    console.log(req.method);
+    // intercept only certain requests
+    // if (req.method === 'DELETE') {
+    //   console.log('DELETE Request is on the way');
+    // }
+
+    // intercept all requests
+    console.log('Request is on the way');
+    return next.handle(req);
+  }
+}
